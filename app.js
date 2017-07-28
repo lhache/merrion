673bdca0aa16e685e9af2affe2c986b66004c1e4
lhache <hache.louis@gmail.com>
2017-07-28 13:08:00 +0200

    add writing to postgres

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require("fs");
+const { Client } = require('pg')
 
 var app = express();
 
@@ -17,6 +18,20 @@ app.get('/', function (req, res) {
 
 app.post('/add', function (req, res) {
   const data = req.body
+
+  const client = new Client({
+    user: 'czclulqxihkkbh',
+    host: 'ec2-50-19-83-146.compute-1.amazonaws.com',
+    database: 'dbdn4hnl7ohqks',
+    password: 'ff7d14a21d1327fa7b213de2aef267c5ef9ecb861b86e9ca83d4373740e023c1',
+    port: 5432,
+    ssl: true
+  })
+
+  client.connect()
+  client.query('INSERT INTO attendees(firstName, lastName, email) values($1, $2, $3)',
+    [data.firstName, data.lastName, data.email]);
+
   const line = `${data.firstName},${data.lastName},${data.email}`
   fs.appendFile(__dirname + "/" + "db", appendNewLine(line), function (err) {
     if (err) throw err;
